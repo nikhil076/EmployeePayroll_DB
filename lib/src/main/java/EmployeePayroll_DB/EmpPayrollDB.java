2446b809d3e85bf3f017432d3823d985d5c6f5b1
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-27 21:12:12 +0530

    Added jdbc connection with the sql and check if the connection working properly

---
+++ lib/src/main/java/EmployeePayroll_DB/EmpPayrollDB.java
@@ -0,0 +1,44 @@
+package EmployeePayroll_DB;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.sql.Connection;
+
+public class EmpPayrollDB {
+	public static List<EmpPayrollData> empPayrollDataList = new ArrayList<>();
+
+
+	public static void main(String args[]) {
+		EmpPayrollDB db = new EmpPayrollDB();
+		String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_services?characterEncoding=utf8";
+		String userName = "root";
+		String password = "1640";
+		Connection connection;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded..");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database :" + jdbcUrl);
+			connection = DriverManager.getConnection(jdbcUrl, userName, password);
+			System.out.println("Connection is successful!" + connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = driverList.nextElement();
+            System.out.println("Driver:" + driverClass.getClass().getName());
+        }
+	}
+}

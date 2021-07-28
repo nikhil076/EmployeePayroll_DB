2dff97285d567da821a7b751e340e065ffca5816
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-28 21:53:46 +0530

    Added test case to check if the sum of salary is valid

--- lib/src/test/java/EmployeePayroll_DB/EmployeePayrollTesting.java
+++ lib/src/test/java/EmployeePayroll_DB/EmployeePayrollTesting.java
@@ -11,29 +11,34 @@ import EmployeePayroll_DB.EmployeePayrollService.IOService;
 public class EmployeePayrollTesting {
 
 	@Test
-    public void given3EmployeesWhenReadFromFileShouldMatchNumberOfEmployeeEntries() {
+	public void given3EmployeesWhenReadFromFileShouldMatchNumberOfEmployeeEntries() {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-		
+
 		employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
 		int result = employeePayrollService.sizeOfEmpList();
 		Assert.assertEquals(4, result);
 	}
-	
+
 	@Test
-	public void givenNewSalaryForEmployee_ShouldMatchWhenUpdated() throws SQLException
-	{
-		EmployeePayrollService  employeePayrollService = new EmployeePayrollService();
+	public void givenNewSalaryForEmployee_ShouldMatchWhenUpdated() throws SQLException {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		List<EmpPayrollData> empPayrollDataList = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
 		employeePayrollService.updateEmpSalary("Mark", 6000000);
 		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Mark");
 		Assert.assertTrue(result);
 	}
-	
+
 	@Test
-	public void givenDateBetweeen_shouldReturnNumberOfvalues()
-	{
+	public void givenDateBetweeen_shouldReturnNumberOfvalues() {
 		EmployeePayrollDBService dbService = new EmployeePayrollDBService();
 		int result = dbService.retriveDataBetweenGivenDate("2018-01-01", "2020-01-01");
 		Assert.assertEquals(1, result);
+	}
+
+	@Test
+	public void givenGender_ShouldReturnSumOfSalary() {
+		EmployeePayrollDBService dbService = new EmployeePayrollDBService();
+		long result = dbService.retiveMaxOfSalaryGroupByGender('M');
+		Assert.assertEquals(16000000, result);
 	}
 }

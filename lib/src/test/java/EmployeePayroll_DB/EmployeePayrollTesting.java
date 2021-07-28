70687f3bb38c6b50cfabbea42f5b545a3929e059
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-28 21:08:56 +0530

    Added the test case to check if updated file is in sync with the list

--- lib/src/test/java/EmployeePayroll_DB/EmployeePayrollTesting.java
+++ lib/src/test/java/EmployeePayroll_DB/EmployeePayrollTesting.java
@@ -1,14 +1,31 @@
 package EmployeePayroll_DB;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import EmployeePayroll_DB.EmployeePayrollService.IOService;
 
 public class EmployeePayrollTesting {
 
 	@Test
     public void given3EmployeesWhenReadFromFileShouldMatchNumberOfEmployeeEntries() {
-		EmployeePayrollDBService dbService = new EmployeePayrollDBService();
-		long result = dbService.readData();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		
+		employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+		int result = employeePayrollService.sizeOfEmpList();
 		Assert.assertEquals(4, result);
+	}
+	
+	@Test
+	public void givenNewSalaryForEmployee_ShouldMatchWhenUpdated() throws SQLException
+	{
+		EmployeePayrollService  employeePayrollService = new EmployeePayrollService();
+		List<EmpPayrollData> empPayrollDataList = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+		employeePayrollService.updateEmpSalary("Mark", 6000000);
+		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Mark");
+		Assert.assertTrue(result);
 	}
 }

08707f3ff7e5d483ee7cef4575071870e83b6b62
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-29 18:21:35 +0530

    Added the new atrributes for payroll details

--- lib/src/main/java/EmployeePayroll_DB/EmpPayrollData.java
+++ lib/src/main/java/EmployeePayroll_DB/EmpPayrollData.java
@@ -1,6 +1,7 @@
 package EmployeePayroll_DB;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class EmpPayrollData {
 
@@ -8,6 +9,10 @@ public class EmpPayrollData {
     public String name;
     public double salary;
     public LocalDate date;
+    public Character gender;
+    public String companyId;
+    public String department[];
+    private boolean isActive = true;
 
     public EmpPayrollData(Integer id, String name, Double salary) {
         this.id = id;
@@ -15,14 +20,112 @@ public class EmpPayrollData {
         this.salary = salary;
     }
 
-    public EmpPayrollData(Integer id, String name, Double salary,LocalDate date) {
+    
+    public EmpPayrollData(int id, String name, double salary, LocalDate date, Character gender, String companyId,
+			String[] department, boolean isActive) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.date = date;
+		this.gender = gender;
+		this.companyId = companyId;
+		this.department = department;
+		this.isActive = isActive;
+	}
+
+
+	public EmpPayrollData(Integer id, String name, Double salary,LocalDate date) {
         this(id,name,salary);
         this.date = date;
     }
 
-    @Override
-    public String toString() {
-        return "EmpPayrollData{" +
-                "employeeId=" + id + ", employeeName=" + name + ", employeeSalary" + salary + ", startDate=" + date + "}";
-    }
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+
+	public Character getGender() {
+		return gender;
+	}
+
+
+	public void setGender(Character gender) {
+		this.gender = gender;
+	}
+
+
+	public String getCompanyId() {
+		return companyId;
+	}
+
+
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+
+
+	public String[] getDepartment() {
+		return department;
+	}
+
+
+	public void setDepartment(String[] department) {
+		this.department = department;
+	}
+
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+
+	@Override
+	public String toString() {
+		return "EmpPayrollData [id=" + id + ", name=" + name + ", salary=" + salary + ", date=" + date + ", gender="
+				+ gender + ", companyId=" + companyId + ", department=" + Arrays.toString(department) + ", isActive="
+				+ isActive + "]";
+	}
+    
 }
